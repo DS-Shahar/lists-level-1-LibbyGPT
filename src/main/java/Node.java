5bbb8b34fc93c0c0ba369c5a2965e2cd7ae5b251
LibbyGPT <libibomiller@gmail.com>
2024-11-30 21:52:33 +0200

    Update Node.java

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,40 +1,40 @@
-package list;
+package List;
 
 public class Node<T> {
-  private T value;
-  private Node<T> next;
+    private T value;
+    private Node<T> next;
 
-  public Node(T value) {
-    this.value = value;
-    this.next = null;
-  }
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
 
-  public Node(T value, Node<T> next) {
-    this.value = value;
-    this.next = next;
-  }
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
 
-  public T getValue() {
-    return value;
-  }
+    public T getValue() {
+        return value;
+    }
 
-  public Node<T> getNext() {
-    return next;
-  }
+    public Node<T> getNext() {
+        return next;
+    }
 
-  public boolean hasNext() {
-    return (next != null);
-  }
+    public boolean hasNext() {
+        return next != null;
+    }
 
-  public void setValue(T value) {
-    this.value = value;
-  }
+    public void setValue(T value) {
+        this.value = value;
+    }
 
-  public void setNext(Node<T> next) {
-    this.next = next;
-  }
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 
-  public String toString() {
-    return value + " --> " + next;
-  }
+    public String toString() {
+        return value + " --> " + next;
+    }
 }
